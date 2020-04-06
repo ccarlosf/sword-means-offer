19da087a35062a783fba919018be5da6d9bcc703
ccarlosf <2238513100@qq.com>
2020-04-06 09:37:40 +0800

    Initial commit

---
+++ src/main/java/com/ccarlos/swordmeansoffer/SwordMeansOfferApplication.java
@@ -0,0 +1,13 @@
+package com.ccarlos.swordmeansoffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordMeansOfferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwordMeansOfferApplication.class, args);
+    }
+
+}
